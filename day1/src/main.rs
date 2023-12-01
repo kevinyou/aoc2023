321b4de3b726b7ef6b8e81ebc95bceb1f086f6e1
Kevin You <kevin.you@outlook.com>
2023-12-01 10:43:49 -0600

    add tests and general refactor

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,5 +1,25 @@
 use std::io::{self, BufRead};
 
+use std::fs;
+
+fn load_file(file_path: &str) -> Vec<String> {
+    let mut lines: Vec<String> = Vec::new();
+    let full_file_string = fs::read_to_string(file_path)
+        .expect("Error while reading file");
+    for line in full_file_string.split("\n"){
+        lines.push(line.to_string());
+    }
+    return lines;
+}
+
+fn load_from_stdin() -> Vec<String> {
+    let stdin = io::stdin();
+    let mut lines = Vec::new();
+    for line in stdin.lock().lines() {
+        lines.push(line.expect("Error while unwrapping a line"));
+    }
+    return lines;
+}
 
 fn parse_digit_word(substr: &str) -> Option<u32> {
     let digit_words = [
@@ -23,15 +43,9 @@ fn parse_digit_word(substr: &str) -> Option<u32> {
     return None;
 }
 
-fn main() {
-    let stdin = io::stdin();
-    let mut lines = stdin.lock().lines();
-
+fn solve(lines: Vec<String>, legacy_part1_flag: bool) -> u32 {
     let mut total_calibration_value = 0;
-
-    while let Some(line) = lines.next() {
-
-        let line = line.unwrap();
+    for line in lines {
         // Trailing newline
         if line.len() == 0 {
             continue;
@@ -39,37 +53,43 @@ fn main() {
 
         let mut first_digit: Option<u32> = None;
         let mut last_digit: Option<u32> = None;
-        'outer: for i in 0..line.len() {
+        for i in 0..line.len() {
             let substr: String = line.chars().take(i + 1).collect();
             let c = substr.chars().last().expect("Getting last char failed");
 
             if c.is_digit(10) {
                 first_digit = c.to_digit(10);
-                break 'outer;
+                break;
             }
 
+            if legacy_part1_flag {
+                continue;
+            }
             match parse_digit_word(&substr) {
                 Some(val) => {
                     first_digit = Some(val);
-                    break 'outer;
+                    break;
                 }
                 None => (),
             }
         }
 
-        'outer: for i in (0..line.len()).rev() {
+        for i in (0..line.len()).rev() {
             let substr: String = line.chars().skip(i).collect();
             let c = substr.chars().nth(0).expect("Getting first char failed");
 
             if c.is_digit(10) {
                 last_digit = c.to_digit(10);
-                break 'outer;
+                break;
             }
 
+            if legacy_part1_flag {
+                continue;
+            }
             match parse_digit_word(&substr) {
                 Some(val) => {
                     last_digit = Some(val);
-                    break 'outer;
+                    break;
                 }
                 None => (),
             }
@@ -81,6 +101,35 @@ fn main() {
         let calibration_value: u32 = calibration_value.parse().expect("not a num");
         total_calibration_value += calibration_value;
     }
+    total_calibration_value
+}
+
+fn main() {
+    // let lines = load_from_stdin();
+    let lines = load_file("./part1.txt");
+
+    let total_calibration_value = solve(lines, false);
 
     println!("{}", total_calibration_value);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(
+            solve(load_file("./example.txt"), true),
+            142
+        );
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(
+            solve(load_file("./example2.txt"), false),
+            281
+        );
+    }
 }
