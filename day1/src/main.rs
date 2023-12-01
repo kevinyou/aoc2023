ce52ff17a0eeea99bc12a8d620bbaf0a0511bdb4
Kevin You <kevin.you@outlook.com>
2023-11-30 23:06:55 -0600

    echo stdin

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,13 @@
+use std::io::{self, BufRead};
+
 fn main() {
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+
+    while let Some(line) = lines.next() {
+        let line = line.unwrap();
+        println!("{}", line);
+    }
+
     println!("Hello, world!");
 }
