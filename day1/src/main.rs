a694b2e1edfe437c1cc6e5eb66d339b3750de4b3
Kevin You <kevin.you@outlook.com>
2023-11-30 23:42:36 -0600

    part2 that does not work because words can have shared characters

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -8,10 +8,28 @@ fn main() {
 
     while let Some(line) = lines.next() {
 
-        let line = line.unwrap();
+        let mut line = line.unwrap();
         // Trailing newline
         if line.len() == 0 {
             continue;
+        }
+
+        // Replace all instances of word with digit
+        let digit_words = [
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+        ];
+
+        for (index, word) in digit_words.iter().enumerate() {
+            let val = (index + 1).to_string();
+            line = line.replace(word, &val);
         }
 
         let first_digit = line.find(|c: char| c.is_digit(10)).expect("no first digit?");
@@ -29,7 +47,8 @@ fn main() {
         let calibration_value = format!("{}{}", first_digit, last_digit);
         let calibration_value: u32 = calibration_value.parse().expect("not a num");
         total_calibration_value += calibration_value;
+    println!("{}", calibration_value);
     }
 
-    println!("{}", total_calibration_value);
+    // println!("{}", total_calibration_value);
 }
