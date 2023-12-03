75fe9f37b1537af5030e9fd0cb50e9e2753f0a05
Kevin You <kevin.you@outlook.com>
2023-12-03 01:02:10 -0600

    solve example but failing real input

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,14 +1,89 @@
 
 use aoc2023::load_file;
+use std::str::FromStr;
 
 static DAYSTRING: &str = "day2";
 
-fn solve_part1(lines: &Vec<String>) -> u32 {
-    let mut total_calibration_value = 0;
-    for line in lines {
-        //
+
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+
+impl FromStr for Color {
+
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Color, Self::Err> {
+        match input {
+            "red" => Ok(Color::Red),
+            "blue" => Ok(Color::Blue),
+            "green" => Ok(Color::Green),
+            _ => Err(()),
+        }
     }
-    total_calibration_value
+}
+
+struct Clue {
+    count: u32,
+    color: Color,
+}
+
+
+fn parse_clue(clue: &str) -> Clue{
+    let parts: Vec<&str> = clue.trim().split(' ').collect();
+    Clue {
+        count: parts[0].parse().expect("Clue count not number"),
+        color: Color::from_str(parts[1]).expect("Clue color not color"),
+    }
+}
+
+fn solve_part1(lines: &Vec<String>) -> u32 {
+    let mut game_id_sum = 0;
+    for line in lines {
+        if line.len() == 0 {
+            continue;
+        }
+        let v: Vec<&str> = line.split(':').collect();
+
+        let game_id = v[0];
+        let game_id: Vec<&str> = game_id.split(' ').collect();
+        let game_id: u32 = game_id[1]
+            .parse()
+            .expect("Game id is not a number");
+
+        let sets = v[1].trim();
+        let sets: Vec<&str> = sets.split(';').collect();
+        let sets: Vec<Vec<Clue>> = sets
+            .into_iter()
+            .map(
+                |set| set.split(", ")
+                .map(parse_clue)
+                .collect()
+            )
+            .collect();
+
+        let mut valid = true;
+        for set in sets {
+            for clue in set {
+                let quantity = match clue.color {
+                    Color::Red => 12,
+                    Color::Blue => 13,
+                    Color::Green => 14,
+                };
+                if clue.count > quantity {
+                    valid = false;
+                    break;
+                }
+            }
+        }
+
+        if valid {
+            game_id_sum += game_id;
+        }
+    }
+    game_id_sum
 }
 
 fn main() {
