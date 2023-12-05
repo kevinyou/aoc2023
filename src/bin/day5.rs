6ccfff298bae950741acdfecda7b9b8884015522
Kevin You <kevin.you@outlook.com>
2023-12-04 20:44:02 -0600

    Prepare day5

---
+++ src/bin/day5.rs
@@ -0,0 +1,50 @@
+
+use aoc2023::load_file;
+
+static DAYSTRING: &str = "day5";
+
+fn solve_part1(lines: &Vec<String>) -> u32 {
+    0
+}
+
+fn solve_part2(lines: &Vec<String>) -> u32 {
+    0
+}
+
+
+fn main() {
+    // let lines = load_from_stdin();
+    let file_path = format!("./data/{DAYSTRING}/part1.txt");
+    let lines = load_file(&file_path);
+
+    let part1 = solve_part1(&lines);
+
+    println!("Part 1: {part1}");
+
+    let part2 = solve_part2(&lines);
+
+    println!("Part 2: {part2}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        let file_path = format!("./data/{DAYSTRING}/example1.txt");
+        assert_eq!(
+            solve_part1(&load_file(&file_path)),
+            1337
+        );
+    }
+
+    #[test]
+    fn test_part_2() {
+        let file_path = format!("./data/{DAYSTRING}/example1.txt");
+        assert_eq!(
+            solve_part2(&load_file(&file_path)),
+            1337
+        );
+    }
+}
